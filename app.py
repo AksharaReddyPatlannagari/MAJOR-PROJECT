a0b2606a5b14324047f661509bc58dd744794d5b
vn1907 <63858944+vn1907@users.noreply.github.com>
2023-02-09 21:59:49 +0530

    update html and flask

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
 @app.route('/sol', methods=['POST','GET'])
 def getDate():
   date=request.form['date']
-  return solar_prediction.dataSetProcessing(date)
+  return ('The total solar generation on '+date+' is:  ' +solar_prediction.dataSetProcessing(date))
 
 if __name__ == '_main_':
     app.run(debug=True)
